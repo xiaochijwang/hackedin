754794d3a29b216e1f2ce8faffcdae7782d883cc
JustinHoUMD <itsjustinn@gmail.com>
2013-11-09 01:10:49 -0500

    Add register and team activities

--- src/com/example/hackedin/RegisterActivity.java
+++ src/com/example/hackedin/RegisterActivity.java
@@ -1,5 +1,21 @@
 package com.example.hackedin;
 
-public class RegisterActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
 
+public class RegisterActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
--- src/com/example/hackedin/TeamActivity.java
+++ src/com/example/hackedin/TeamActivity.java
@@ -1,5 +1,22 @@
 package com.example.hackedin;
 
-public class TeamActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
 
+public class TeamActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_team);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
